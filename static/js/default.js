7970f7053fbd49ee00da14c2f0005687227ada3f
25th-Night <brown_eyed87@naver.com>
2023-06-01 18:27:57 +0900

    Add show&add todo function

--- static/js/default.js
+++ static/js/default.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    showTodo();
+});
+
 
 var cardList = document.querySelectorAll('.card');
 cardList = [].slice.call(cardList)
@@ -11,24 +15,43 @@ if (cardList) {
     });
 } else {};
 
-var storageTodoData = new Array();
-
 var section = document.querySelector('.card-list');
+
+function gridTodo(content) {
+    var card = document.createElement("div");
+    card.classList.add("card");
+    card.classList.add("card");
+    card.innerHTML = `<div class="card-body d-flex justify-content-between align-items-center">
+                        <div class="todo" data-bs-toggle="tooltip" data-bs-placement="top">${content}</div>
+                        <button class="btn btn-danger">Remove</button>
+                    </div>`
+    section.insertBefore(card, section.firstChild);
+}
 
 function addOnTodo() {
     var input = document.querySelector(".form-control");
     var inputData = input.value;
-    console.log(inputData)
-    var card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `<div class="card-body d-flex justify-content-between align-items-center">
-                        <div class="todo" data-bs-toggle="tooltip" data-bs-placement="top">${inputData}</div>
-                        <button class="btn btn-danger">Remove</button>
-                    </div>`
+
+    gridTodo(inputData)
+
     section.insertBefore(card, section.firstChild);
-    storageTodoData.push(inputData);
-    localStorage.setItem("ToDo", storageTodoData)
+
+    var existingData = localStorage.getItem("card");
+    if (existingData) {
+        inputData = inputData + "," + existingData;
+    }
+    localStorage.setItem("card", inputData);
+
     input.value = "";
+}
+
+
+function showTodo() {
+    var existingData = localStorage.getItem("card");
+    var inputList = existingData.split(",");
+    inputList.forEach((inputData) => {
+        gridTodo(inputData)
+    })
 }
 
 const addOnBtn = document.getElementById("button-addon");
