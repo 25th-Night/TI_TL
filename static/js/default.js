ed6b3d089920bda0843d0911c8f60fba56bc44d6
25th-Night <brown_eyed87@naver.com>
2023-06-01 17:50:10 +0900

    Add addOnTodo js function

--- static/js/default.js
+++ static/js/default.js
@@ -1,4 +1,35 @@
-var todo = document.querySelector('.todo');
-var tooltip = new bootstrap.Tooltip(todo, {
-    title: todo.innerText
-});
+
+var cardList = document.querySelectorAll('.card');
+cardList = [].slice.call(cardList)
+
+if (cardList) {
+    cardList.forEach((card) => {
+        var todo = card.querySelector('.todo');
+        var tooltip = new bootstrap.Tooltip(todo, {
+            title: todo.innerText
+        });
+    });
+} else {};
+
+var storageTodoData = new Array();
+
+var section = document.querySelector('.card-list');
+
+function addOnTodo() {
+    var input = document.querySelector(".form-control");
+    var inputData = input.value;
+    console.log(inputData)
+    var card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `<div class="card-body d-flex justify-content-between align-items-center">
+                        <div class="todo" data-bs-toggle="tooltip" data-bs-placement="top">${inputData}</div>
+                        <button class="btn btn-danger">Remove</button>
+                    </div>`
+    section.insertBefore(card, section.firstChild);
+    storageTodoData.push(inputData);
+    localStorage.setItem("ToDo", storageTodoData)
+    input.value = "";
+}
+
+const addOnBtn = document.getElementById("button-addon");
+addOnBtn.addEventListener("click", addOnTodo);
