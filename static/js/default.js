d5aced5c0114c83c3164595b716b77485a04e55e
25th-Night <brown_eyed87@naver.com>
2023-06-01 19:09:39 +0900

    Fix tooltip code and variable name

--- static/js/default.js
+++ static/js/default.js
@@ -1,44 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
-    showTodo();
+    readTodo();
 });
-
-
-var cardList = document.querySelectorAll('.card');
-cardList = [].slice.call(cardList)
-
-if (cardList) {
-    cardList.forEach((card) => {
-        var todo = card.querySelector('.todo');
-        var tooltip = new bootstrap.Tooltip(todo, {
-            title: todo.innerText
-        });
-    });
-} else {};
 
 var section = document.querySelector('.card-list');
 
 function gridTodo(content) {
     var card = document.createElement("div");
     card.classList.add("card");
-    card.classList.add("card");
     card.innerHTML = `<div class="card-body d-flex justify-content-between align-items-center">
                         <div class="todo" data-bs-toggle="tooltip" data-bs-placement="top">${content}</div>
                         <button class="btn btn-danger">Remove</button>
                     </div>`
+
+    var todo = card.querySelector('.todo');
+    var tooltip = new bootstrap.Tooltip(todo, {
+        title: todo.innerText
+    });
+
     section.insertBefore(card, section.firstChild);
+
+    return card;
 }
 
-function addOnTodo() {
+function createTodo() {
     var input = document.querySelector(".form-control");
     var inputData = input.value;
 
-    gridTodo(inputData)
+    var card = gridTodo(inputData);
 
     section.insertBefore(card, section.firstChild);
 
     var existingData = localStorage.getItem("card");
     if (existingData) {
-        inputData = inputData + "," + existingData;
+        inputData = existingData + "," + inputData;
     }
     localStorage.setItem("card", inputData);
 
@@ -46,13 +40,15 @@ function addOnTodo() {
 }
 
 
-function showTodo() {
+function readTodo() {
     var existingData = localStorage.getItem("card");
     var inputList = existingData.split(",");
+    console.log(inputList);
     inputList.forEach((inputData) => {
         gridTodo(inputData)
     })
 }
 
-const addOnBtn = document.getElementById("button-addon");
-addOnBtn.addEventListener("click", addOnTodo);
+
+var createBtn = document.getElementById("button-addon");
+createBtn.addEventListener("click", createTodo);
